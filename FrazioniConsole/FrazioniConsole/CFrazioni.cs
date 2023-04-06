ef033c9ae7dbc4994d38099cd52793f9d0915c2d
Aleksandar69188 <9698118@itisrossi.vi.it>
2023-04-06 20:31:23 +0200

    Primo test frazioni

---
+++ FrazioniConsole/FrazioniConsole/CFrazioni.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrazioniConsole
+{
+    class CFrazione
+    {
+        private int _num, _den; // usabili solo in questa classe
+
+        public int num // usabili anche da fuori = property dell'oggetto
+        {
+            get
+            {
+                return _num;
+            }
+            set
+            {
+                _num = value;
+            }
+        }
+        // servono per controllare se i valori dati in input sono accettabili
+        // 
+        public int den
+        {
+            get
+            {
+                return _den;
+            }
+            set
+            {
+                if (value != 0)
+                {
+                    _den = value;
+                }
+                else
+                {
+                    _den = 1;
+                }
+            }
+        }
+
+        public CFrazione() //costruttore di default
+        {
+            num = 0; //è come mettere this.num = 0; 
+            den = 0; //con la property me lo mette lui a 1
+        }
+
+        public CFrazione(int num, int den) //overloading costruttore = diamo più informazioni
+        {
+            this.num = num; // num property è uguale a num parametro
+            this.den = den;
+        }
+
+        public override string ToString() // da warning perchè è già presente in ogni classe il metodo ToString = regalo
+        { // override per sovrascrivere e togliere il warning :)
+            string risultato = "";
+            if (this.den < 0)
+            {
+                this.den = Math.Abs(this.den);
+                this.num = -this.num;
+            }
+            risultato = this.num.ToString() + "/" + this.den.ToString();
+            return risultato;
+        }
+
+        // Somma
+        public static CFrazione Somma(CFrazione f1, CFrazione f2) // static = di classe
+        {
+            CFrazione ris;
+            ris = new CFrazione();
+            ris.den = f1.den * f2.den;
+            ris.num = (ris.den / f1.den) * f1.num + (ris.den / f2.den) * f2.num;
+            return ris;
+        }
+
+        public CFrazione Somma(CFrazione f1) // da fare sull'oggetto
+        {
+            CFrazione ris = CFrazione.Somma(this, f1); //per non ricopiare il codice // this = l'oggetto su cui applico la funzione
+            return ris;
+        }
+
+        //override dell'operatore +
+        public static CFrazione operator +(CFrazione f1, CFrazione f2) // static = di classe
+        {
+            CFrazione ris = CFrazione.Somma(f1, f2);
+            return ris;
+        }
+
+        //Sottrazione
+        public static CFrazione Sottrazione(CFrazione f1, CFrazione f2) // static = di classe
+        {
+            CFrazione ris;
+            ris = new CFrazione();
+            ris.den = f1.den * f2.den;
+            ris.num = (ris.den / f1.den) * f1.num - (ris.den / f2.den) * f2.num;
+            return ris;
+        }
+
+        public CFrazione Sottrazione(CFrazione f1) // da fare sull'oggetto
+        {
+            CFrazione ris = CFrazione.Sottrazione(this, f1);
+            return ris;
+        }
+
+        //override dell'operatore -
+        public static CFrazione operator -(CFrazione f1, CFrazione f2) // static = di classe
+        {
+            CFrazione ris = CFrazione.Sottrazione(f1, f2);
+            return ris;
+        }
+
+        //Moltiplicazione
+        public static CFrazione Moltiplicazione(CFrazione f1, CFrazione f2) // static = di classe
+        {
+            CFrazione ris;
+            ris = new CFrazione();
+            ris.den = f1.den * f2.den;
+            ris.num = f1.num * f2.num;
+            return ris;
+        }
+
+        public CFrazione Moltiplicazione(CFrazione f1) // da fare sull'oggetto
+        {
+            CFrazione ris = CFrazione.Moltiplicazione(this, f1);
+            return ris;
+        }
+
+        //override dell'operatore *
+        public static CFrazione operator *(CFrazione f1, CFrazione f2) // static = di classe
+        {
+            CFrazione ris = CFrazione.Moltiplicazione(f1, f2);
+            return ris;
+        }
+
+        //Divisione
+        public static CFrazione Divisione(CFrazione f1, CFrazione f2) // static = di classe
+        {
+            CFrazione ris = new CFrazione();
+            f2 = f2.Inverti();
+            ris.den = f1.den * f2.den;
+            ris.num = f1.num * f2.num;
+            return ris;
+        }
+
+        public CFrazione Inverti()
+        {
+            CFrazione ris = new CFrazione();
+            ris.den = this.num;
+            ris.num = this.den;
+            return ris;
+        }
+
+        public CFrazione Divisione(CFrazione f1) // da fare sull'oggetto
+        {
+            CFrazione ris = CFrazione.Divisione(this, f1);
+            return ris;
+        }
+
+        //override dell'operatore /
+        public static CFrazione operator /(CFrazione f1, CFrazione f2) // static = di classe
+        {
+            CFrazione ris = CFrazione.Divisione(f1, f2);
+            return ris;
+        }
+
+        public CFrazione Semplifica() // da fare sull'oggetto
+        {
+            CFrazione ris = new CFrazione();
+            int temp = MCD(this.num, this.den);
+            ris.num = this.num / temp;
+            ris.den = this.den / temp;
+            return ris;
+        }
+
+        private static int MCD(int n1, int n2)
+        {
+            n1 = Math.Abs(n1);
+            n2 = Math.Abs(n2);
+            while (n1 != 0 && n2 != 0)
+            {
+                if (n1 > n2)
+                    n1 %= n2;
+                else
+                    n2 %= n1;
+            }
+            return n1 | n2;
+        }
+    }
+}
---
+++ FrazioniConsole/FrazioniConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrazioniConsole
+{
+    class Program
+    {
+        static void Main()
+        {
+            CFrazione f1 = new CFrazione(3, 0), f2 = new CFrazione(1, 0);
+            Console.WriteLine("Frazione 1 {0}; Frazione 2 {1}; Somma {2}", f1.ToString(), f2.ToString(), CFrazione.Somma(f1, f2).Semplifica());
+            Console.WriteLine("Sottrazione {0}; Moltiplicazione {1}, Divisione {2}", CFrazione.Sottrazione(f1, f2), (f1 * f2).Semplifica(), f1.Divisione(f2));
+            Console.ReadLine();
+        }
+    }
+}
